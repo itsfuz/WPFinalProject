d29fd9fd426c421f52e96b4622cfa100b7f59614
A <70055599+thelonedoodle@users.noreply.github.com>
2022-01-03 22:51:26 +0700

    fixed the css

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{URL::asset('/resources/css/jhfurniture.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('/css/jhfurniture.css')}}">
     <title>JH Furniture</title>
 </head>
 <body>
